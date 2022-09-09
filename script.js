f8f1c82a18333d2411074a68d90c250b1a02ce78
Mark Silva <mark.allen.silva@gmail.com>
2022-09-08 20:59:33 -0700

    computations work

--- script.js
+++ script.js
@@ -22,15 +22,44 @@ class Calculator {
   }
 
   chooseOperation(operation) {
-  
+    if (this.currentOperand === '') return;
+    if (this.previousOperand != '') {
+      this.compute(); // this conditional places the computed value in the prevOperand div and allows for multiple operations
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = '';
   }
 
   compute() {
-
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        computation = prev + current;
+        break;
+      case '-':
+        computation = prev - current;
+        break;
+      case '*':
+        computation = prev * current;
+        break;
+      case '/':
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
   }
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand; 
+    this.previousOperandTextElement.innerText = this.previousOperand; 
   }
 }
 
@@ -57,3 +86,13 @@ operationButtons.forEach(button => {
     calculator.updateDisplay();
   });
 });
+
+equalButton.addEventListener('click', button => {
+  calculator.compute();
+  calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button => {
+  calculator.clear();
+  calculator.updateDisplay();
+})
